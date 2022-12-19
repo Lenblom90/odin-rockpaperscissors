b9fb7ff5211a83c5279788fc6a250eb9ee775498
lblom <lenblom@gmail.com>
2022-12-19 15:30:42 +0200

    Setup initial files
    Created a index.html file and javascript file with the instructions
    in pseudocode.

---
+++ script.js
@@ -0,0 +1,22 @@
+function getComputerChoice(){
+    // random return of "Rock", "Paper" or "Scissors"
+    // Log the choice to the console
+}
+
+function playRound(playerSelection, computerSelection) {
+    // make playerSelection case insensitive
+    // Return "You Lose! Paper Beats Rock"
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+function game(){
+    for (let i = 0; i <5; i++) {
+    // using playRound, play a 5 round game
+    // keep score
+    // report a winner and loser at the end
+    }
+}
