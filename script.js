5e4e615bd5de1bdb5650e47f31006578c67a8bf2
lblom <lenblom@gmail.com>
2022-12-20 16:26:03 +0200

    Create UI with buttons and display results
    Input is now changed to buttons for all 3 choices with eventlisteners
    that call playRound. Computer's choice and results are displayed as text

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 function getComputerChoice(){
     const choices = ["Rock", "Paper", "Scissors"];
     let randomNumber = Math.floor(Math.random() * 3);
-    console.log("Computer choice: " + choices[randomNumber])
+    resultDisplay.textContent = "Computer choice: " + choices[randomNumber] + ". ";
     return choices[randomNumber];
-    // random return of "Rock", "Paper" or "Scissors"
-    // Log the choice to the console
 }
 
-function playRound(playerSelection, computerSelection) {
-    // make playerSelection case insensitive
-    let playerChoice = playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase();
-
-    // Return result
+function playRound(playerChoice, computerSelection) {
     let winMessage = `You Win! ${playerChoice} Beats ${computerSelection}`;
     let loseMessage =  `You Lose! ${computerSelection} Beats ${playerChoice}`;
     let tieMessage = "It's a Tie!";
@@ -30,12 +24,14 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-/*
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-*/
+const buttons = document.querySelectorAll("button");
+const resultDisplay = document.querySelector(".results");
+buttons.forEach(btn => btn.addEventListener('click', () => {
+    let result = playRound(btn.textContent, getComputerChoice())
+    resultDisplay.textContent = resultDisplay.textContent + result;
+}));
 
+/*
 function game(){
     let playerScore = 0;
     let computerScore = 0;
@@ -70,3 +66,4 @@ function game(){
 }
 
 game();
+*/
